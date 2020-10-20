30c04c32a8d5c2ac6f7781de622df067ada398ac
lishuchun <lishuchun @shuchun.li>
2020-10-20 16:48:27 +0800

    名片管理

---
+++ card_manager/card_main.py
@@ -0,0 +1,35 @@
+card_info = []
+
+if __name__ == '__main__':
+
+    while True:
+        # 显示功能菜单
+        print('*' * 50)
+        print("欢迎使用【名片管理系统】V1.0")
+        print("1.新建名片")
+        print("2.显示全部")
+        print("3.查询名片")
+        print()
+        print("0.退出系统")
+        print('*' * 50)
+
+        action_str = input("请选择操作功能：")
+
+        if action_str in ['1', '2', '3']:
+            # 1新建名片
+            if action_str == '1':
+                print(1)
+            # 2显示全部
+            elif action_str == '2':
+                print(2)
+            # 3查询名片
+            elif action_str == '3':
+                print(3)
+        elif action_str == '0':
+            print('退出系统,欢迎下次使用')
+            break
+        else:
+            print("您输入的不正确，请重新选择")
+
+
+
---
+++ card_manager/card_tool.py
@@ -0,0 +1,4 @@
+def create_card(card_info):
+    card_personal_info = {}
+
+    
